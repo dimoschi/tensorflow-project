c9bc218012d75a0e722be4a55e60552f5a4e5470
Dimosthenis Schizas <dimoschi@gmail.com>
2017-07-26 15:02:36 +0300

    Changes in images methods: * Generalized get_images to be used for both train and test * Added batch_size, instead of counters * Pick images randomly for each batch * Added function to "fix" images

--- main.py
+++ main.py
@@ -8,75 +8,72 @@ Created on Mon Jul 17 20:03:13 2017
 from __future__ import print_function
 
 import os
+import random
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 
 # Parameters
-learning_rate = 0.001
-training_iters = 100
-batch_size = 3
-display_step = 1
+KEY_PARAMETERS = {
+    "learning_rate": 0.001,
+    "training_iters": 100,
+    "batch_size": 6,
+    "display_step": 1,
+    "required_size": (259, 194),  # tuple of required size
+    "n_input": 259*194*3,  # data input (img shape)
+    "n_classes": 2,  # total classes (0-1: uncensored - censored)
+    "dropout": 0.75,  # Dropout, probability to keep units
+}
 
-# Network Parameters
-size = [259, 194]
-n_input = 259*194*3  # data input (img shape)
-n_classes = 2  # total classes (0-1: uncensored - censored)
-dropout = 0.75  # Dropout, probability to keep units
 
 # tf Graph input
-x = tf.placeholder(tf.float32, shape=[None, n_input])
-y = tf.placeholder(tf.float32, shape=[None, n_classes])
+x = tf.placeholder(tf.float32, shape=[None, KEY_PARAMETERS["n_input"]])
+y = tf.placeholder(tf.float32, shape=[None, KEY_PARAMETERS["n_classes"]])
 keep_prob = tf.placeholder(tf.float32)  # dropout (keep probability)
 
 
 # IMPORT images
 # TODO: discard bad images
-def get_images(censored_counter, uncensored_counter):
+def get_images(batch_size=10, folder="train"):
     images = []
-    y = []
-    path_censored = os.path.join(os.getcwd(), "input", "train", "censored")
-    path_uncensored = os.path.join(os.getcwd(), "input", "train", "uncensored")
-    for i in range(uncensored_counter, uncensored_counter+2):
-        im = Image.open(os.path.join(path_uncensored, "{}.jpg".format(i)))
-        if im.mode != "RGB":
-            im = im.convert("RGB")
-        images.append(im.resize(size))
-        y.append([0, 1])
-    im = Image.open(os.path.join(
-        path_censored, "{}.jpg".format(censored_counter)
-    ))
-    if im.mode != "RGB":
-        im = im.convert("RGB")
-    images.append(im.resize(size))
-    y.append([1, 0])
-    return images, y
+    images_labels = []
+
+    path_censored = os.path.join(os.getcwd(), "input", folder, "censored")
+    path_uncensored = os.path.join(os.getcwd(), "input", folder, "uncensored")
+
+    uncensored = round(batch_size*0.8)
+    censored = batch_size - uncensored
+    for image in random.sample(os.listdir(path_uncensored), uncensored):
+        im = Image.open(os.path.join(path_uncensored, image))
+        images.append(fix_image(im))
+        images_labels.append([0, 1])
+    for image in random.sample(os.listdir(path_censored), censored):
+        im = Image.open(os.path.join(path_censored, image))
+        images.append(fix_image(im))
+        images_labels.append([1, 0])
+    return images, images_labels
 
 
-def get_test_images():
-    images = []
-    y = []
-    path_censored = os.path.join(os.getcwd(), "input", "test", "censored")
-    path_uncensored = os.path.join(os.getcwd(), "input", "test", "uncensored")
-    censored = os.listdir(path_censored)
-    uncensored = os.listdir(path_uncensored)
-    for file in uncensored:
-        im = Image.open(os.path.join(path_uncensored, file))
-        if im.mode != "RGB":
-            im = im.convert("RGB")
-        images.append(im.resize(size))
-        y.append([0, 1])
-    for file in censored:
-        im = Image.open(os.path.join(path_censored, file))
-        if im.mode != "RGB":
-            im = im.convert("RGB")
-        images.append(im.resize(size))
-        y.append([1, 0])
-    return images, y
+def fix_image(image):
+    """
+    Check if an image needs to be resized and/or
+    converted from grayscale to RGB
+
+    Args:
+        image: An image as PIL.Images
+
+    Returns:
+        image: An image as PIL.Image in specific size and in RGB
+    """
+    required_size = KEY_PARAMETERS["required_size"]
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    if image.size != required_size:
+        image = image.resize(required_size, Image.LANCZOS)
+    return image
 
 
-def get_batch(images, y):  # batch_size
-    # images, y = get_images(batch_size)
+def get_batch(images, y):
     batch_x = np.asarray([np.asarray(
         image, dtype=np.float32).flatten() for image in images])
     batch_y = np.asarray(y)
@@ -102,29 +99,28 @@ def maxpool2d(x, k=2):
 def conv_net(x, weights, biases, dropout):
     # Reshape input picture
     x = tf.reshape(x, shape=[-1, 194, 259, 3])
-    #
     # Convolution Layer
     conv1 = conv2d(x, weights['wc1'], biases['bc1'])
     # Max Pooling (down-sampling)
     conv1 = maxpool2d(conv1, k=2)
+    print(conv1.get_shape())
     #
     # Convolution Layer
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    #conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
     # Max Pooling (down-sampling)
-    conv2 = maxpool2d(conv2, k=2)
+    #conv2 = maxpool2d(conv2, k=2)
     # print(conv2.get_shape())
-    # conv3 = conv2d(conv2, weights['wc3'], biases['bc3'])
+    #conv3 = conv2d(conv2, weights['wc3'], biases['bc3'])
     # print(conv3.get_shape())
     #
     # Fully connected layer
     # Reshape conv2 output to fit fully connected layer input
     # fc1 = tf.reshape(conv1, [-1, weights['wd1'].get_shape().as_list()[0]])
-    fc1 = tf.reshape(conv2, [-1, 49*65*64])
+    fc1 = tf.reshape(conv1, [-1, 403520])
     fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
     fc1 = tf.nn.relu(fc1)
     # Apply Dropout
     fc1 = tf.nn.dropout(fc1, dropout)
-    #
     # Output, class prediction
     out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
     return out
@@ -132,15 +128,15 @@ def conv_net(x, weights, biases, dropout):
 
 # Store layers weight & bias
 weights = {
-    # 5x5 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.random_normal([7, 2, 3, 32])),
+    # 7x2 conv, 3 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([2, 7, 3, 32])),
     # 5x5 conv, 32 inputs, 64 outputs
     'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
     'wc3': tf.Variable(tf.random_normal([3, 3, 64, 32])),
     # fully connected, 7*7*64 inputs, 1024 outputs
-    'wd1': tf.Variable(tf.random_normal([49*65*64, 1024])),
+    'wd1': tf.Variable(tf.random_normal([403520, 1024])),
     # 1024 inputs, 10 outputs (class prediction)
-    'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    'out': tf.Variable(tf.random_normal([1024, KEY_PARAMETERS["n_classes"]]))
 }
 
 biases = {
@@ -148,7 +144,7 @@ biases = {
     'bc2': tf.Variable(tf.random_normal([64])),
     'bc3': tf.Variable(tf.random_normal([32])),
     'bd1': tf.Variable(tf.random_normal([1024])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
+    'out': tf.Variable(tf.random_normal([KEY_PARAMETERS["n_classes"]]))
 }
 
 # Construct model
@@ -158,7 +154,9 @@ pred = conv_net(x, weights, biases, keep_prob)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=pred, labels=y)
 )
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+optimizer = tf.train.AdamOptimizer(
+    learning_rate=KEY_PARAMETERS["learning_rate"]
+).minimize(cost)
 
 # Evaluate model
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
@@ -171,43 +169,39 @@ init = tf.global_variables_initializer()
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
-    censored_count = 1
-    uncensored_count = 1
+    step = 1
     # Keep training until reach max iterations
-    while (censored_count < 101) and (uncensored_count < 201):
-        images, classes = get_images(censored_count, uncensored_count)
-        batch_x, batch_y = get_batch(images, classes)
+    while step < 11:
+        batch_size = KEY_PARAMETERS["batch_size"]
+        dropout = KEY_PARAMETERS["dropout"]
+        images, classes = get_images(batch_size, "train")
+        train_batch_x, train_batch_y = get_batch(images, classes)
         # Run optimization op (backprop
         sess.run(optimizer, feed_dict={
-            x: batch_x,
-            y: batch_y,
+            x: train_batch_x,
+            y: train_batch_y,
             keep_prob: dropout
         })
-        if censored_count % 5 == 0:
+        if step % 2 == 0:
             # Calculate batch loss and accuracy
             loss, acc = sess.run([cost, accuracy], feed_dict={
-                x: batch_x,
-                y: batch_y,
+                x: train_batch_x,
+                y: train_batch_y,
                 keep_prob: dropout
             })
             # Calculate accuracy for 256 mnist test images
-            test_images, test_y = get_test_images()
-            test_x, test_y = get_batch(test_images, test_y)
+            test_images, test_y = get_images(20, "test")
+            test_batch_x, test_batch_y = get_batch(test_images, test_y)
             t_acc = sess.run(accuracy, feed_dict={
-                x: test_x,
-                y: test_y,
+                x: test_batch_x,
+                y: test_batch_y,
                 keep_prob: 1.
             })
             print(
-                "Iter " + str(censored_count) +
+                "Iter " + str(step) +
                 ", Minibatch Loss= " + "{:.6f}".format(loss) +
                 ", Training Accuracy= " + "{:.5f}".format(acc) +
                 ", Testing Accuracy= " + "{:.5f}".format(t_acc)
             )
-            print(
-                "Censored Count:" + str(censored_count) +
-                ", Uncensored Count:" + str(uncensored_count)
-            )
-        censored_count += 1
-        uncensored_count += 2
+        step += 1
     print("Optimization Finished!")
